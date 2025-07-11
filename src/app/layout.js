e3afb2f00afa9cb04136371761720031f679a679
ayushh-gautam <ayushgautam896@gmail.com>
2025-07-11 13:49:58 +0545

    footer completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./navBar/page";
+import Footer from "./footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         <NavBar />
 
         {children}
+        <Footer />
       </body>
     </html>
   );
