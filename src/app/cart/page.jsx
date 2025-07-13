097ff23d318f92ccb647f42e42fbfd83f37b978d
ayushh-gautam <ayushgautam896@gmail.com>
2025-07-13 13:38:31 +0545

    cart screen

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Cart() {
   return (
     <>
-      <section className="text-3xl text-white border  bg-[#211C24] h-[100vh] w-full z-50">
-        <div className="w-[1200px] bg-slate-400  mx-auto flex  items-center justify-center">
+      <section className="text-3xl text-white border  bg-[#211C24] h-[100vh] w-full z-[60px]">
+        <div className="max-w-[1200px] bg-slate-400   m-auto flex flex-col  items-center justicenterfy-around">
+          <h1>jahsauheuwuweyuwegyu</h1>
           <h1>jahsauheuwuweyuwegyu</h1>
         </div>
       </section>
