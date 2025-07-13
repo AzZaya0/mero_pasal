c43b59c1e38db039e14089f93ccb9b4f42b3e231
ayushh-gautam <ayushgautam896@gmail.com>
2025-07-13 13:39:09 +0545

    minor fix

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -4,7 +4,7 @@ function Cart() {
   return (
     <>
       <section className="text-3xl text-white border  bg-[#211C24] h-[100vh] w-full z-[60px]">
-        <div className="max-w-[1200px] bg-slate-400   m-auto flex flex-col  items-center justicenterfy-around">
+        <div className="max-w-[1200px] bg-slate-400   m-auto flex flex-col  items-center justify-around">
           <h1>jahsauheuwuweyuwegyu</h1>
           <h1>jahsauheuwuweyuwegyu</h1>
         </div>
