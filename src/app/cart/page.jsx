4e0999478dd8fdc58dba34ebc477f10480a74742
ayushh-gautam <ayushgautam896@gmail.com>
2025-07-14 17:08:54 +0545

    responsive made

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -30,22 +30,22 @@ function Cart() {
   const fadeInRight = {
     initial: { x: 100, opacity: 0 },
     whileInView: { x: 0, opacity: 1 },
-    transition: { duration: 0.6, ease: "easeOut", delay: 0.1 },
+    transition: { duration: 0.6, ease: "easeOut", delay: 0 },
     viewport: { amount: 0.4, once: true },
   };
 
   return (
     <section className="text-lg text-black border border-white flex justify-center items-center  h-[100vh] w-full z-[60px] overflow-y-auto">
-      <div className="max-w-[1200px] w-full mx-auto gap-24 flex flex-row  justify-between p-6">
+      <div className="max-w-[1200px] w-full mx-auto flex flex-col lg:flex-row gap-20">
         {/* Left: Cart Items */}
         <motion.div
           initial={fadeInLeft.initial}
           whileInView={fadeInLeft.whileInView}
           transition={fadeInLeft.transition}
           viewport={fadeInLeft.viewport}
-          className="flex flex-col gap-4 w-[50%] "
+          className="flex flex-col gap-4 w-full px-4 lg:w-1/2"
         >
-          <h1 className="mb-4 text-[24px] font-semibold">Shopping Cart</h1>
+          <h1 className="mb-4 text-2xl font-semibold">Shopping Cart</h1>
           {cartItems.map((item, index) => (
             <CartLayout
               key={index}
@@ -64,46 +64,48 @@ function Cart() {
           whileInView={fadeInRight.whileInView}
           transition={fadeInRight.transition}
           viewport={fadeInRight.viewport}
-          className="bg-white text-black p-4 border rounded flex flex-col w-[50%] px-16 py-14"
+          className="bg-white text-black p-6 border rounded w-full lg:w-1/2"
         >
+          <h1 className="text-xl font-semibold mb-4">Order Summary</h1>
           <div>
-            <h1 className="text-xl font-semibold mb-4">Order Summary</h1>
-            <p className="text-[#545454] text-[14px] pt-[24px]">
+            <label className="text-[#545454] text-sm block mt-6 mb-1">
               Discount code / Promo code
-            </p>
+            </label>
             <input
               type="text"
               placeholder="code"
               className="w-full p-2 border border-gray-300 rounded"
             />
-            <p className="text-[#545454] text-[14px] pt-[24px]">
+
+            <label className="text-[#545454] text-sm block mt-6 mb-1">
               Your bonus card number
-            </p>
+            </label>
             <input
               type="text"
               placeholder="Enter card number"
               className="w-full p-2 border border-gray-300 rounded"
             />
-            {/* Order Summary Details */}
-            <div className="flex justify-between items-center pt-8 text-[16px]">
-              <div className="flex flex-col gap-2">
-                <p>Subtotal</p>
-                <p className="text-[#545454]">Estimated tax</p>
-                <p className="text-[#545454]">Estimated shipping & Handling</p>
-                <p>Total</p>
-              </div>
-              <div className="flex flex-col gap-2 font-medium">
-                <p>$1750</p>
-                <p>$150</p>
-                <p>$50</p>
-                <p>$1950</p>
-              </div>
-            </div>
-
-            <button className="bg-black w-full text-white text-[16px] font-medium py-3 px-4 rounded mt-6">
-              Checkout
-            </button>
           </div>
+
+          {/* Order Summary Details */}
+          <div className="flex justify-between items-start pt-8 text-base">
+            <div className="flex flex-col gap-2">
+              <p>Subtotal</p>
+              <p className="text-[#545454]">Estimated tax</p>
+              <p className="text-[#545454]">Shipping & Handling</p>
+              <p className="font-semibold">Total</p>
+            </div>
+            <div className="flex flex-col gap-2 font-medium text-right">
+              <p>$1750</p>
+              <p>$150</p>
+              <p>$50</p>
+              <p className="font-bold">$1950</p>
+            </div>
+          </div>
+
+          <button className="bg-black w-full text-white text-base font-medium py-3 px-4 rounded mt-6">
+            Checkout
+          </button>
         </motion.div>
       </div>
     </section>
